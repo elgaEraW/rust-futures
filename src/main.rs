3843d29da92efbc56386067f8d2905d0e0b31905
Dev Joshi <devjoshi030@gmail.com>
2024-09-05 00:06:12 +0530

    Sample Executor and Spawner for a TimerFuture - TimerFuture implements a Future and marks itself ready after 2sec - Future should wait and then using Waker call the poll till it receives   a Poll::Ready(()) - On a Poll::Pending case, it should assign a waker to call the poll   method to imply that Future can make progress again.

---
+++ src/main.rs
@@ -0,0 +1,91 @@
+use std::{
+  sync::{
+    mpsc::{sync_channel, Receiver, SyncSender},
+    Arc, Mutex,
+  },
+  task::Context,
+  time::Duration,
+};
+
+use futures::{
+  future::BoxFuture,
+  task::{waker_ref, ArcWake},
+  Future, FutureExt,
+};
+use timer::TimerFuture;
+
+struct Executor {
+  ready_queue: Receiver<Arc<Task>>,
+}
+
+impl Executor {
+  fn run(&self) {
+    while let Ok(task) = self.ready_queue.recv() {
+      let mut future_slot = task.future.lock().unwrap();
+      if let Some(mut future) = future_slot.take() {
+        let waker = waker_ref(&task);
+        let context = &mut Context::from_waker(&waker);
+
+        if future.as_mut().poll(context).is_pending() {
+          *future_slot = Some(future);
+        }
+      }
+    }
+  }
+}
+
+#[derive(Clone)]
+struct Spawner {
+  task_sender: SyncSender<Arc<Task>>,
+}
+
+impl Spawner {
+  fn spawn(&self, future: impl Future<Output = ()> + 'static + Send) {
+    let future = future.boxed();
+    let task = Arc::new(Task {
+      future: Mutex::new(Some(future)),
+      task_sender: self.task_sender.clone(),
+    });
+    self.task_sender.send(task).expect("too many tasks queued");
+  }
+}
+
+struct Task {
+  future: Mutex<Option<BoxFuture<'static, ()>>>,
+  task_sender: SyncSender<Arc<Task>>,
+}
+
+impl ArcWake for Task {
+  fn wake_by_ref(arc_self: &Arc<Self>) {
+    let cloned = arc_self.clone();
+    arc_self
+      .task_sender
+      .send(cloned)
+      .expect("too many tasks queued");
+  }
+}
+
+fn new_executor_and_spawner() -> (Executor, Spawner) {
+  const MAX_QUEUED_TASKS: usize = 10_000;
+
+  let (task_sender, ready_queue) = sync_channel(MAX_QUEUED_TASKS);
+  (Executor { ready_queue }, Spawner { task_sender })
+}
+
+fn main() {
+  let (executor, spawner) = new_executor_and_spawner();
+
+  spawner.spawn(async {
+    println!("aa");
+
+    for _ in 0..11_000 {
+      TimerFuture::new(Duration::from_micros(200)).await;
+    }
+
+    println!("bb");
+  });
+
+  drop(spawner);
+
+  executor.run();
+}
